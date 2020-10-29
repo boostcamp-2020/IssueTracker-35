08e5250ccd8985ac3ef9d55ff6f161313be32e11
권영언 <kyu9341@naver.com>
2020-10-29 14:00:34 +0900

    [FEAT] gitbub 로그인을 위한 라우터 추가
    기존 라우터에서 연결되는 컨트롤러의 메소드 이름 변경

--- server/main/routes/users.js
+++ server/main/routes/users.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const userController = require("../controllers/user");
+const express = require('express');
+const userController = require('../controllers/user');
 const router = express.Router();
 
-router.get("/login/github", userController.githubLogin);
+router.get('/login/github', userController.getGithubLoginUrl);
+router.post('/login/github', userController.githubLogin);
 
 module.exports = router;
