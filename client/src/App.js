daabed3451921b9f5920da3b6066856ac6ee5a90
joh16 <math_98@naver.com>
2020-10-29 16:43:11 +0900

    [FEAT] code를 사용해 서버로부터 token 받아오기
    github callback 이후 처리

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  BrowserRouter
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import LoginContainer from './LoginContainer';
+import { LoginContainer, GitHubCallback } from './auth';
+const IssueContainer = null; // import IssueContainer from './issue';
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
   return (
-    <div>
-      <Route path="/" component={LoginContainer} />
-    </div>
+    <Switch>
+      <Route path="/" exact component={token === undefined ? IssueContainer : LoginContainer} />
+      <Route path="/users/github/callback" cb={setToken} component={GitHubCallback} />
+    </Switch>
   );
 };
 
 export default App;
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
