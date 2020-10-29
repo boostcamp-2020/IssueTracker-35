4ed71abb8176580bbbbe32853346dd3155055b3b
권영언 <kyu9341@naver.com>
2020-10-29 14:02:31 +0900

    [FEAT] githubLogin에서 access_token을 받아오는 함수 구현
    클라이언트에서 보낸 code와 client_id, client_secret로 사용자 정보에 접근 하기 위한 access_token을 받아오는 함수 구현

--- server/main/controllers/user.js
+++ server/main/controllers/user.js
@@ -1,12 +1,43 @@
 const { responseHandler } = require('../utils/handler');
+const axios = require('axios');
 
 class UserController {
-
-  async githubLogin(req, res, next) {
+  async getGithubLoginUrl(req, res, next) {
     const clientID = process.env.CLIENT_ID;
     const url = `https://github.com/login/oauth/authorize?client_id=${clientID}`;
 
     responseHandler(res, 200, { url });
+  }
+
+  async githubLogin(req, res, next) {
+    const { code } = req.body;
+
+    const getAccessToken = async code => {
+      const tokenUrl = 'https://github.com/login/oauth/access_token';
+
+      const data = {
+        code,
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+      };
+
+      const config = {
+        headers: { accept: 'application/json' },
+      };
+
+      try {
+        const {
+          data: { access_token },
+        } = await axios.post(tokenUrl, data, config);
+
+        if (!access_token) throw Error();
+
+        return access_token;
+      } catch (err) {
+        throw Error(err);
+      }
+    };
+
   }
 }
 
