36390f8db5abecceef0544fb77f4cbaa6929a362
joh16 <math_98@naver.com>
2020-10-29 14:33:16 +0900

    [FEAT] react router 적용
    / 경로에서 loginContainer가 보이도록 함

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from 'react-router-dom';
+
 import LoginContainer from './LoginContainer';
 
 const App = () => {
   return (
     <div>
-      <p>React here!</p>
-      <LoginContainer></LoginContainer>
+      <Route path="/" component={LoginContainer} />
     </div>
   );
 };
 
 export default App;
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
