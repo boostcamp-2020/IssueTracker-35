b2274d87caa9dbfa1cd514ce9ffb68815597b663
권영언 <kyu9341@naver.com>
2020-11-02 02:01:06 +0900

    [STYLE] lint 적용

--- client/src/const/url.js
+++ client/src/const/url.js
@@ -1,5 +1,3 @@
-const BASE_URL = process.env.NODE_ENV === "production"
-    ? "http://118.67.132.70:3000"
-    : "http://127.0.0.1:3000";
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://118.67.132.70:3000' : 'http://127.0.0.1:3000';
 
 export default BASE_URL;
