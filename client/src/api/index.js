731aa72b7790c7d19cc3eed55395ba53dc3258c9
권영언 <kyu9341@naver.com>
2020-11-02 01:55:36 +0900

    [REFACTOR] api 를 한 곳에서 관리할 수 있도록 변경
    get, post, put, patch, delete를 위한 axios 추상화

---
+++ client/src/api/index.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import BASE_URL from '@/const/url';
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    Authorization: localStorage.getItem('token'),
+  },
+});
+
+export const requestGET = async (url, params = {}, headers = {}) => {
+  try {
+    return await instance.get(url, { params, headers });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const requestPOST = async (url, data = {}, headers = {}) => {
+  try {
+    return await instance.post(url, data, headers);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const requestPUT = async (url, data = {}, headers = {}) => {
+  try {
+    return await instance.put(url, data, headers);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const requestPATCH = async (url, data = {}, headers = {}) => {
+  try {
+    return await instance.patch(url, data, headers);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const requestDELETE = async (url, params = {}, headers = {}) => {
+  try {
+    return await instance.delete(url, { params, headers });
+  } catch (err) {
+    console.log(err);
+  }
+};
