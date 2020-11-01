f47ea869c5b72cd4ba3bd6613cbb7b123b9822c5
권영언 <kyu9341@naver.com>
2020-11-01 16:04:18 +0900

    [FEAT] test용 user seed 작성
    테스트를 위한 user seed파일 추가

---
+++ server/test/seeds/user.js
@@ -0,0 +1,32 @@
+const { User } = require('../server/models');
+
+const expectedUser = {
+  id: 4,
+  nickname: "user123",
+  password: "useruser",
+};
+
+const initUsers = () => {
+  const users = [
+    {
+      id: 1,
+      nickname: "user11",
+      password: "useruser",
+    },
+    {
+      id: 2,
+      nickname: "user22",
+      password: "useruser",
+    },
+    {
+      id: 3,
+      nickname: "user33",
+      password: "useruser",
+    },
+  ];
+
+  User.destory({ where: {} });
+  User.bulkCreate(users);
+};
+
+module.exports = { initUsers, expectedUser };
