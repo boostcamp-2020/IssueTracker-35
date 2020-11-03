236ef81d171b9c1b6afb16da5ac2e7f765648dd4
dimpl@JinYoung <dimple0416@naver.com>
2020-11-03 18:52:24 +0900

    [REFACTOR] Styled-Component로 리팩토링 Styled-Component 적용 및 svgs 디렉토리로 github logo 분리

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import { LoginContainer, GitHubCallback } from './auth';
 const IssueContainer = null; // import IssueContainer from './issue';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    position: relative;
+    margin: 0;
+    height: 100vh;
+    background-color: #F6F6F6;
+  }
+`;
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token'));
 
   return (
-    <Switch>
-      <Route path="/" exact component={!token ? LoginContainer : IssueContainer} />
-      <Route path="/users/github/callback" render={props => <GitHubCallback {...props} cb={setToken} />} />
-    </Switch>
+    <>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact component={!token ? LoginContainer : IssueContainer} />
+        <Route path="/users/github/callback" render={props => <GitHubCallback {...props} cb={setToken} />} />
+      </Switch>
+    </>
   );
 };
 
