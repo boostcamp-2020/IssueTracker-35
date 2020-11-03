9ad64181b878e03da25f837bfaebc7cdab2a87ec
권영언 <kyu9341@naver.com>
2020-11-04 00:55:25 +0900

    [FEAT] utils에 password 암호화하기 위한 함수 작성

--- server/main/utils/auth.js
+++ server/main/utils/auth.js
@@ -1,4 +1,10 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+exports.hashingPw = password => bcrypt.hash(password, saltRounds);
+
+exports.comparePw = (password, dbPassword) => bcrypt.compare(password, dbPassword);
 
 exports.createJWT = user => {
   const { id, nickname } = user;
