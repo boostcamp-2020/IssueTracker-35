6b82c5be25e26a8c391d384f45a077cb77a9e976
dimpl@JinYoung <dimple0416@naver.com>
2020-11-03 17:56:24 +0900

    [REFACTOR] Styled Tag 및 색상 정보 작업

---
+++ client/src/styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  LIGHT_GREEN: '#54A158', // new issue button
+  LIGHT_BLUE: '#2D6CD0', // login / register button
+  GHOST_WHITE: '#F6F6F6', // close issue button
+  LIGHT_GRAY: '#D8DBDF', // input border
+  GRAY: '#A0A0A0', // sign in github
+  CLOUDY_WHITE: '#FAFBFC', // login background
+  DARK_WHITE: '#F6F8FA', // issue comment
+  LIGHT_SKYBLUE: '#F2F8FE', // issue owner
+  LIGHT_BLACK: '#25292E', // navi bar
+  BLUE: '#2B67CF', // picked label
+};
+
+export default colors;
