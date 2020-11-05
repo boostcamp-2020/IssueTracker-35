1fd24938f56143bb172d2d7fc3ea8b804e879e08
권영언 <kyu9341@naver.com>
2020-11-06 00:41:00 +0900

    [CHORE] 주석 제거

--- server/main/services/issue.js
+++ server/main/services/issue.js
@@ -18,19 +18,6 @@ class IssueService {
           model: Assignment,
           attributes: ['assignee'],
           as: 'assignment',
-
-          //   include: [
-          //     {
-          //       model: Issue,
-          //       attributes: ['user_id'],
-          //       as: 'issue',
-          //     },
-          //     {
-          //       model: User,
-          //       attributes: ['nickname', 'image'],
-          //       as: 'user',
-          //     },
-          //   ],
         },
       ],
     });
