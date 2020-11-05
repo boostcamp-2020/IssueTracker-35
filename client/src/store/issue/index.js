a1994715b19e1212168f2c129056b6e88f64058e
권영언 <kyu9341@naver.com>
2020-11-05 21:30:25 +0900

    [FEAT] issue reducer 구현, issue 더미데이터 추가

--- client/src/store/issue/index.js
+++ client/src/store/issue/index.js
@@ -1,12 +1,42 @@
 import React, { createContext, useReducer } from 'react';
 
+import reducer from './reducer';
+
 const IssueContext = createContext();
 
-const reducer = (state, action) => {
-  return state;
+const initState = {
+  issues: [
+    {
+      id: 1,
+      title: '첫 번째 이슈입니다.',
+      content: '첫 번째 내용~~',
+      is_open: true,
+      createdAt: new Date('2020-09-02 17:00'),
+      author: { nickname: '망했어요' },
+      comment_count: 0,
+    },
+    {
+      id: 2,
+      title: '두 번째 이슈입니다.',
+      content: '두 번째 내용~~',
+      is_open: false,
+      author: { nickname: '엉엉' },
+      createdAt: new Date('2020-11-05 18:20'),
+      comment_count: 2,
+    },
+    {
+      id: 3,
+      title: '세 번째 이슈입니다.',
+      content: '세 번째 내용~~',
+      is_open: true,
+      author: { nickname: '퇴근 마렵다' },
+      createdAt: new Date('2020-11-05 18:57'),
+      comment_count: 3,
+    },
+  ],
+  selected: [],
+  timestamp: new Date(),
 };
-
-const initState = {};
 
 const IssueProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initState);
