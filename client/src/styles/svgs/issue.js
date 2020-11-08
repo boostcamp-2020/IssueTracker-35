66b682f4a6c382a687a542e730b0f2a0d9ec1527
권영언 <kyu9341@naver.com>
2020-11-08 16:48:40 +0900

    [STYLE] 기본 배경 색 변경, 스네이크 케이스로 된 변수 -> 카멜 케이스로 변경

--- client/src/styles/svgs/issue.js
+++ client/src/styles/svgs/issue.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import colors from '../colors';
 
-const IssueIcon = ({ is_open }) => {
+const IssueIcon = ({ isOpen }) => {
   return (
     <svg
       viewBox="0 0 16 16"
@@ -10,9 +10,9 @@ const IssueIcon = ({ is_open }) => {
       width="16"
       height="16"
       aria-hidden="true"
-      fill={is_open ? colors.LIGHT_GREEN : colors.DARK_RED}
+      fill={isOpen ? colors.LIGHT_GREEN : colors.DARK_RED}
     >
-      {is_open ? (
+      {isOpen ? (
         <path
           fillRule="evenodd"
           d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm9 3a1 1 0 11-2 0 1 1 0 012 0zm-.25-6.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
