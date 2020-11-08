dcc481a07625f74b9b0fee332c643bc801442458
권영언 <kyu9341@naver.com>
2020-11-09 00:29:41 +0900

    [FEAT] 이슈 검색 및 필터링 UI 일부 구현

---
+++ client/src/containers/issue/search.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+import color from '@/styles/colors';
+import { Button, Input, Div } from '@/styles/styled';
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 5px;
+  height: 1rem;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const FilterButton = styled.button`
+  border: 1px solid ${color.LIGHT_GRAY2};
+  border-right: none;
+  border-radius: 5px 0 0 5px;
+  height: 30px;
+  padding: 5px 16px;
+  font-size: 14px;
+  font-weight: 500;
+  white-space: nowrap;
+  cursor: pointer;
+  &:hover {
+    background-color: ${color.LIGHT_GRAY};
+  }
+`;
+
+const SearchInput = styled(Input)`
+  border: 1px solid ${color.LIGHT_GRAY2};
+  border-radius: 0 5px 5px 0;
+  height: 30px;
+  width: 100%;
+`;
+
+const ButtonContainer = styled(Div.row)`
+  margin: 0px 1rem;
+`;
+
+const LabelButton = styled(FilterButton)`
+  background-color: transparent;
+`;
+
+const MileStoneButton = styled(FilterButton)`
+  background-color: transparent;
+  border-radius: 0 5px 5px 0;
+  border: 1px solid ${color.LIGHT_GRAY2};
+`;
+
+const NewIssueButton = styled(Button)`
+  font-size: 14px;
+  padding: 5px 16px;
+  white-space: nowrap;
+  height: 30px;
+`;
+
+const SearchContainer = () => {
+  return (
+    <Container>
+      <FilterContainer>
+        <FilterButton>Filters &#9662;</FilterButton>
+        <SearchInput></SearchInput>
+      </FilterContainer>
+      <ButtonContainer>
+        <LabelButton>Labels</LabelButton>
+        <MileStoneButton>Milestones</MileStoneButton>
+      </ButtonContainer>
+      <Link to="/issues/new">
+        <NewIssueButton>New issue</NewIssueButton>
+      </Link>
+    </Container>
+  );
+};
+
+export default SearchContainer;
