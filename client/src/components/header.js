1ad4fceebfe7d015fec4831e1b48606b10c0147f
권영언 <kyu9341@naver.com>
2020-11-10 17:12:43 +0900

    [FEAT] 로그아웃 기능 구현

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import color from '@/styles/colors';
 import size from '@/styles/sizes';
 import { Div } from '@/styles/styled';
 import { Link } from 'react-router-dom';
+import { UserContext } from '@/store/user';
+import { LOGOUT } from '@/store/user/actions';
 
 const StyledHeader = styled.header`
   background-color: ${color.LIGHT_BLACK};
@@ -13,7 +15,6 @@ const StyledHeader = styled.header`
   box-shadow: 0 1px 6px rgba(0, 0, 0, 0.5);
   position: fixed;
   align-items: center;
-  justify-content: center;
   margin: 0;
   top: 0;
   color: ${color.WHITE};
@@ -22,19 +23,41 @@ const StyledHeader = styled.header`
 `;
 
 const Logo = styled.button`
+  position: absolute;
   background-color: transparent;
   color: ${color.WHITE};
   border: none;
   outline: none;
   cursor: pointer;
+  left: 50%;
+`;
+
+const Logout = styled(Logo)`
+  left: 90%;
+`;
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
 `;
 
 const Header = () => {
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(UserContext);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    dispatch({ type: LOGOUT });
+  };
+
   return (
     <StyledHeader>
-      <Link to="/">
+      <StyledLink to="/">
         <Logo>ISSUES</Logo>
-      </Link>
+      </StyledLink>
+      {user && <Logout onClick={logoutHandler}>LOGOUT</Logout>}
     </StyledHeader>
   );
 };
