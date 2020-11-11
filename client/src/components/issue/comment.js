788edb1993c0be6a9a40d8355b22ac9d4b7c77a8
권영언 <kyu9341@naver.com>
2020-11-11 10:47:45 +0900

    [FEAT] #52 코멘트 헤더 추가

--- client/src/components/issue/comment.js
+++ client/src/components/issue/comment.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import color from '@/styles/colors';
+import size from '@/styles/sizes';
+import { Div } from '@/styles/styled';
+import getTimestamp from '@/utils/timestamp';
 
 const Container = styled.div`
   display: flex;
@@ -25,6 +28,31 @@ const ContentContainer = styled.div`
   flex: 1 1;
   border: 1px solid ${color.LIGHT_GRAY};
   border-radius: 7px;
+`;
+
+const Header = styled(Div.row)`
+  justify-content: space-between;
+  border-bottom: 1px solid ${color.LIGHT_GRAY};
+  padding: 10px;
+  background-color: ${color.THIN_GRAY};
+`;
+
+const HeaderLeft = styled(Div.row)`
+  justify-content: space-between;
+`;
+
+const Nickname = styled.span`
+  font-size: ${size.DEFAULT_FONT_SIZE};
+  font-weight: 600;
+  margin-right: 10px;
+`;
+
+const Timestamp = styled.span`
+  font-size: ${size.DEFAULT_FONT_SIZE};
+  font-weight: 400;
+`;
+
+const Body = styled.div`
   padding: 10px;
 `;
 
@@ -36,7 +64,17 @@ const Comment = ({ issue }) => {
         src="https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4"
         alt=""
       />
-      <ContentContainer></ContentContainer>
+      <ContentContainer>
+        <Header>
+          <HeaderLeft>
+            <Nickname>{issue?.author.nickname}</Nickname>
+            <Timestamp>
+              commented {getTimestamp(new Date(), issue?.createdAt)}
+            </Timestamp>
+          </HeaderLeft>
+        </Header>
+        <Body></Body>
+      </ContentContainer>
     </Container>
   );
 };
