55daed3befb60ea4cdf4a691068495e90fc0a365
권영언 <kyu9341@naver.com>
2020-11-11 23:58:01 +0900

    [FEAT] #50 assignee modal 구현 (사이드바 드롭다운 모달)

--- client/src/components/issue/assignee.js
+++ client/src/components/issue/assignee.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import color from '@/styles/colors';
 import size from '@/styles/sizes';
-import CheckedSvg from '@/styles/svgs/check';
+import CheckMark from '@/styles/svgs/check';
+import { userAPI } from '@/api/user';
 
 const Container = styled.div`
   width: 100%;
@@ -28,22 +29,96 @@ const Nickname = styled.span`
   font-size: ${size.DEFAULT_FONT_SIZE};
 `;
 
-const CheckMark = styled(CheckedSvg)`
-  display: none;
-`;
+const Assignee = ({ checked, setChecked }) => {
+  const [users, setUsers] = useState([]);
 
-const Assignee = ({ assignees }) => {
+  const getUsers = async () => {
+    /* const {
+       data: { users },
+    } = await userAPI.getAllUsers(); */
+
+    setUsers(initState);
+  };
+
+  useEffect(getUsers, []);
+
+  const handleCheck = (isChecked, userId) => {
+    if (isChecked) {
+      const newChecked = new Set(checked);
+      newChecked.delete(userId);
+      return setChecked(newChecked);
+    }
+    setChecked(new Set([...checked, userId]));
+  };
+
   return (
     <>
-      {assignees.map(assignee => (
-        <Container key={assignee.id}>
-          <CheckMark />
-          <ProfileImage src={assignee.image} />
-          <Nickname>{assignee.nickname}</Nickname>
-        </Container>
-      ))}
+      {users.map(user => {
+        const isChecked = checked.has(user.id);
+        return (
+          <Container
+            key={user.id}
+            onClick={() => handleCheck(isChecked, user.id)}
+          >
+            <CheckMark isChecked={isChecked} />
+            <ProfileImage src={user.image} />
+            <Nickname>{user.nickname}</Nickname>
+          </Container>
+        );
+      })}
     </>
   );
 };
+
+const initState = [
+  {
+    id: 1,
+    nickname: 'aaa',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 2,
+    nickname: 'bbb',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 3,
+    nickname: 'ccc',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 4,
+    nickname: 'dds',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 5,
+    nickname: 'aaa',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 7,
+    nickname: 'bbb',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 8,
+    nickname: 'ccc',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+  {
+    id: 6,
+    nickname: 'dds',
+    image:
+      'https://avatars0.githubusercontent.com/u/49153756?s=460&u=a475983d60adb9ddac3d55771bde039d545360dd&v=4',
+  },
+];
 
 export default Assignee;
