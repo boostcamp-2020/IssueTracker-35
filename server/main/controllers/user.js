55e62c6c665b53d8fea9b1ce28edf31b53cf0c5c
dimpl@JinYoung <dimple0416@naver.com>
2020-11-11 17:52:17 +0900

    [FEAT] #17 테스트 코드에 따른 API 작성

--- server/main/controllers/user.js
+++ server/main/controllers/user.js
@@ -1,22 +1,27 @@
 const { responseHandler } = require('@/utils/handler');
 const { createJWT } = require('@/utils/auth');
+const { userService } = require('@/services/index');
 
 class UserController {
-  async getGithubLoginUrl(req, res, next) {
+  async getGithubLoginUrl(req, res) {
     const clientID = process.env.CLIENT_ID;
     const url = `https://github.com/login/oauth/authorize?client_id=${clientID}`;
 
     responseHandler(res, 200, { url });
   }
 
-  async generateToken(req, res, next) {
+  async generateToken(req, res) {
     const { user } = req;
     const token = createJWT(user);
 
     const { id, nickname, image } = user;
     responseHandler(res, 200, { token, id, nickname, image });
   }
-
+  async getAllUsers(req, res) {
+    const users = await userService.retrieveAllUsers();
+    const responseData = { users: Object.values(users) };
+    responseHandler(res, 200, responseData);
+  }
   async getOwnInfo(req, res) {
     const { user } = req;
     responseHandler(res, 200, user);
