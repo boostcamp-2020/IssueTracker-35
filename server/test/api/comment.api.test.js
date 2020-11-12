3692b1a7583d0ac53ab5207d1f5f732aac2b4366
joh16 <math_98@naver.com>
2020-11-13 01:29:57 +0900

    [TEST] #71 작성자만 수정 가능

--- server/test/api/comment.api.test.js
+++ server/test/api/comment.api.test.js
@@ -5,15 +5,10 @@ const { createJWT } = require('@/utils/auth');
 const { users } = require('@test/seeds/user');
 const { expectedComments } = require('@test/seeds/comment');
 const { Comment } = require('@/models');
-
-const CODE = {
-  CREATED: 201,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-};
+const { status } = require('@test/api/response-status');
 
 describe('create comment', () => {
-  const CREATE_URL = issueId => `/issues/:${issueId}/comments`;
+  const CREATE_URL = issueId => `/issues/${issueId}/comments`;
 
   // given
   const comment = {
@@ -32,15 +27,18 @@ describe('create comment', () => {
           // then
           if (err) throw err;
           const { id, code } = res.body;
-          expect(code).toBe(CODE.CREATED);
-          const receivedComment = await Comment.findByPk(id);
-          expect(receivedComment).toStrictEqual({
+          expect(code).toBe(status.code.CREATED);
+          const receivedComment = await Comment.findByPk(id, {
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+          });
+          expect(receivedComment.dataValues).toStrictEqual({
             id,
             is_issue: false,
             content: comment.content,
             user_id: users[INDEX.user].id,
             issue_id: expectedComments[INDEX.comment].id,
           });
+          await Comment.destroy({ where: { id } });
           done();
         });
     } catch (err) {
@@ -56,7 +54,7 @@ describe('create comment', () => {
         .end((err, res) => {
           if (err) throw err;
           const { code } = res.body;
-          expect(code).toBe(CODE.BAD_REQUEST);
+          expect(code).toBe(status.code.BAD_REQUEST);
           done();
         });
     } catch (err) {
@@ -68,11 +66,12 @@ describe('create comment', () => {
     try {
       request(app)
         .post(CREATE_URL(expectedComments[INDEX.comment].issue_id))
+        .set('Authorization', createJWT(users[INDEX.user]))
         .send({ content: [1, 3] })
         .end((err, res) => {
           if (err) throw err;
           const { code } = res.body;
-          expect(code).toBe(CODE.BAD_REQUEST);
+          expect(code).toBe(status.code.BAD_REQUEST);
           done();
         });
     } catch (err) {
@@ -88,7 +87,97 @@ describe('create comment', () => {
         .end((err, res) => {
           if (err) throw err;
           const { code } = res.body;
-          expect(code).toBe(CODE.UNAUTHORIZED);
+          expect(code).toBe(status.code.UNAUTHORIZED);
+          done();
+        });
+    } catch (err) {
+      done(err);
+    }
+  });
+});
+
+describe('Update comment', () => {
+  const UPDATE_URL = id => `/comments/${id}`;
+  const getOwner = user_id => users.find(user => user.id === user_id);
+
+  // given
+  const INDEX = { comment: 0 };
+  const comment = { content: '바뀌었지롱 ㅗ' };
+
+  it('successfully', done => {
+    try {
+      // when
+      const expectedComment = {
+        ...expectedComments[INDEX.comment],
+        ...comment,
+      };
+      const { id } = expectedComment;
+      request(app)
+        .patch(UPDATE_URL(id))
+        .set('Authorization', createJWT(getOwner(expectedComment.user_id)))
+        .send(comment)
+        .end(async (err, res) => {
+          // then
+          if (err) throw err;
+          const { code } = res.body;
+          expect(code).toBe(status.code.SUCCESS);
+          const receivedComment = await Comment.findByPk(id, {
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
+          });
+          expect(receivedComment.dataValues).toStrictEqual(expectedComment);
+          done();
+        });
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('without content -> 400 Bad request', done => {
+    const expectedComment = expectedComments[INDEX.comment];
+    try {
+      request(app)
+        .patch(UPDATE_URL(expectedComment.id))
+        .set('Authorization', createJWT(getOwner(expectedComment.user_id)))
+        .end((err, res) => {
+          if (err) throw err;
+          const { code } = res.body;
+          expect(code).toBe(status.code.BAD_REQUEST);
+          done();
+        });
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('with wrong ID -> 404 Not Found', done => {
+    try {
+      request(app)
+        .patch(UPDATE_URL('ASDF'))
+        .set('Authorization', createJWT(getOwner(users[0].id)))
+        .end((err, res) => {
+          if (err) throw err;
+          const { code } = res.body;
+          expect(code).toBe(status.code.NOT_FOUND);
+          done();
+        });
+    } catch (err) {
+      done(err);
+    }
+  });
+
+  it('by someone else -> 403 Forbidden', done => {
+    const expectedComment = expectedComments[INDEX.comment];
+    try {
+      request(app)
+        .patch(UPDATE_URL(expectedComment.id))
+        .set(
+          'Authorization',
+          createJWT(users.find(user => user.id !== expectedComment.user_id))
+        )
+        .end((err, res) => {
+          if (err) throw err;
+          const { code } = res.body;
+          expect(code).toBe(status.code.FORBIDDEN);
           done();
         });
     } catch (err) {
