8d43d0402d537af37acf2b43894439f1a329aab9
joh16 <math_98@naver.com>
2020-11-13 00:54:43 +0900

    [FEAT] #70 #71 코멘트 컨트롤러 구현

--- server/main/controllers/comment.js
+++ server/main/controllers/comment.js
@@ -1,20 +1,36 @@
 const { responseHandler } = require('@/utils/handler');
 const { commentService } = require('@/services');
 
-const { convertObjectKeys } = require('@/utils/api');
-const { sequelize } = require('@/models');
-
 class CommentController {
-  async createComment(req, res, next) {
+  constructor() {
+    this.createComment = this.createComment.bind(this);
+    this.updateComment = this.updateComment.bind(this);
+  }
+
+  validate(body, id) {
     const err = new Error('Bad Request');
     err.status = 400;
 
-    if (!req.body && typeof req.body.content !== 'string') return next(err);
-    responseHandler(res, 200, {});
+    if (!body || typeof body.content !== 'string' || isNaN(id)) return err;
+  }
+
+  async createComment(req, res, next) {
+    const err = this.validate(req.body, req.params.issueID);
+    if (err) return next(err);
+    const id = await commentService.create(
+      req.body.content,
+      req.params.issueID,
+      req.user.id
+    );
+
+    responseHandler(res, 201, { id });
   }
 
   async updateComment(req, res, next) {
-    responseHandler(res, 200, {});
+    const err = this.validate(req.body, req.params.commentID);
+    if (err) return next(err);
+    await commentService.update(req.body.content, req.params.commentID);
+    responseHandler(res, 200);
   }
 }
 
